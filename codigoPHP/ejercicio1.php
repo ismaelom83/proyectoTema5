5588b6a41a08770e57102681f0fc268a04741c1f
ismael <ismael.hersal@educa.jcyl.es>
2019-11-28 09:37:51 +0100

    version 191127 0.1
    Conflicts:
    	.gitignore

--- codigoPHP/ejercicio1.php
+++ codigoPHP/ejercicio1.php
@@ -5,20 +5,19 @@
             header('WWW-Authenticate: Basic realm="Dominio crocretil (ismael/paso)"');
             header('HTTP/1.0 401 Unauthorized');
             echo "Operacion cancelada<br>";
-            ?>
-           
-            <?php
+          
             exit;
         } else if (($_SERVER["PHP_AUTH_USER"] == "ismael") && ($_SERVER["PHP_AUTH_PW"] == "paso")) {
-            
+             echo "Usuario: " . $_SERVER['PHP_AUTH_USER'] . "<br/>";
+               echo "Password: " . $_SERVER['PHP_AUTH_PW'];
+               echo '<br>';
                 ?>
                 <a class="btn btn-primary" href="ejercicio0.php">Variables super globales</a>
                 <a class="btn btn-info" href="../tema5.php">VOLVER</a>
                 <h2>Autenticacion con exito</h2>
                 <?php
                 
-                echo "Usuario: " . $_SERVER['PHP_AUTH_USER'] . "<br/>";
-               echo "Password: " . $_SERVER['PHP_AUTH_PW'];
+               
             }
         
         ?>
