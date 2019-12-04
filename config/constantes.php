24883c5ca28b57447ff5491a4d538b83429832a7
ismael <ismaelom83@gmail.es>
2019-12-05 00:44:00 +0100

    developer 191205 practicamente terminado.

--- config/constantes.php
+++ config/constantes.php
@@ -2,7 +2,7 @@
 define('BASEDATOS', 'DAW209DBProyectoTema5');
 define('PASSWD', 'paso');
 define('USUARIO', 'usuarioDAW209DBProyectoTema5');
-define("MAQUINA", "mysql:host=daw209.sauces.local;dbname=DAW209DBProyectoTema5");
-define("URL", "daw209.sauces.local");
+define("MAQUINA", "mysql:host=192.168.1.245;dbname=DAW209DBProyectoTema5");
+define("URL", "192.168.1.245");
 ?>
 
